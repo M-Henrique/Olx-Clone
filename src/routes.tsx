e53ba233aa9e0a9a66cb0c8aa7a933c299354e73
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-07-06 18:29:09 -0300

    Criação da página não encontrada

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import NotFound from './pages/NotFound';
 
 const Routes = () => {
    return (
-      <>
+      <Switch>
          <Route component={Home} path="/" exact></Route>
          <Route component={Sobre} path="/sobre"></Route>
-      </>
+         <Route component={NotFound}></Route>
+      </Switch>
    );
 };
 
